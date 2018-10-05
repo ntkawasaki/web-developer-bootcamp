ae6bcce8aef4ef3d2f6097a3b7d1f7d8d5ded5a3
Noah Kawasaki <ntkawasaki@gmail.com>
2018-10-05 06:54:58 -0700

    Mongoose work

--- part_11_yelp_camp/app.js
+++ part_11_yelp_camp/app.js
@@ -5,15 +5,15 @@ const port = 3000;
 
 var campgrounds = [{
     name: "Salmon Creak",
-    image: "https://pixabay.com/get/ea36b70928f21c22d2524518b7444795ea76e5d004b014429df2c47caeedb3_340.jpg"
+    image: "https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104496f8c57ba5e4bdb9_340.jpg"
   },
   {
     name: "Granite Hill",
-    image: "https://pixabay.com/get/e83db50a21f4073ed1584d05fb1d4e97e07ee3d21cac104496f8c37ca2e4b5be_340.jpg"
+    image: "https://pixabay.com/get/e83db40e28fd033ed1584d05fb1d4e97e07ee3d21cac104496f8c57ba5e4bdb9_340.jpg"
   },
   {
     name: "Big Basin",
-    image: "https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104496f8c37ca2e4b5be_340.jpg"
+    image: "https://pixabay.com/get/e83db50929f0033ed1584d05fb1d4e97e07ee3d21cac104496f8c57ba5e4bdb9_340.jpg"
   }
 ];
 
